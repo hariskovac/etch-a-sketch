85670c3b8c1811676961599a8fe0a3ad53996346
Haris Kovac <hariskovac19@gmail.com>
2022-11-29 19:11:59 -0600

    Add mouseover event listener to change color

--- etch.js
+++ etch.js
@@ -3,3 +3,11 @@ for (let i = 0; i < 256; i++) {
   newDiv.classList.add("hover");
   document.querySelector(".container").appendChild(newDiv);
 }
+
+const squares = document.querySelectorAll(".hover");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseover", function(e) {
+    e.target.style.backgroundColor = "crimson";
+  });
+});
