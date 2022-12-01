50428ac31752188c8462922cdd2e137bd4f90f3b
Haris Kovac <hariskovac19@gmail.com>
2022-11-30 18:00:11 -0600

    Split setGrid into 2 functions

--- etch.js
+++ etch.js
@@ -24,16 +24,16 @@ gridButton.addEventListener("click", () => {
   setGrid();
 })
 
-function setGrid() {
+function getInput() {
   let gridSize = 0;
 
   do {
     gridSize = parseInt(prompt("Please enter a new value between 1 and 100"));
-    console.log(typeof gridSize);
-    console.log(gridSize);
   } while (gridSize > 100 || gridSize < 1 || !(gridSize));
+}
 
-  for (let i = 0; i < Math.pow(gridSize, 2); i++) {
+function setGrid(size) {
+  for (let i = 0; i < Math.pow(size, 2); i++) {
     const newDiv = document.createElement("div");
     document.querySelector(".container").appendChild(newDiv);
   };
