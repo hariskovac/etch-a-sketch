11d9d893d0dd4e5f74be4190abcaf10d9636f8d6
Haris Kovac <hariskovac19@gmail.com>
2022-12-03 18:10:07 -0600

    Rename variables to improve code readability

--- etch.js
+++ etch.js
@@ -9,6 +9,32 @@ sizeSlider.addEventListener("change", () => {
   setGrid(sizeSlider.value);
 })
 
+// Removes the current canvas and creates a new one
+function createCanvas() {
+  const etchBorder = document.querySelector(".etch-border");
+  etchBorder.removeChild(document.querySelector(".canvas"));
+
+  const newCanvas = document.createElement("div");
+  newCanvas.setAttribute("class", "canvas");
+
+  const options = document.querySelector(".options");
+
+  newCanvas.style.gridTemplateColumns = `repeat(${sizeSlider.value}, 1fr)`;
+  newCanvas.style.gridTemplateRows = `repeat(${sizeSlider.value}, 1fr)`;
+  etchBorder.insertBefore(newCanvas, options);
+}
+
+// Creates divs to fill the canvas based on user selected size
+function setGrid(size) {
+  for (let i = 0; i < Math.pow(size, 2); i++) {
+    const canvasElement = document.createElement("div");
+    canvasElement.classList.add("square");
+    canvasElement.addEventListener("mouseover", changeColor);
+    canvasElement.addEventListener("mousedown", changeColor);
+    document.querySelector(".canvas").appendChild(canvasElement);
+  };
+}
+
 // Changes the color of the divs inside the canvas
 function changeColor(e) {
   if (e?.buttons === 1) {
@@ -16,62 +42,12 @@ function changeColor(e) {
   }
 }
 
-// Removes the current canvas and creates a new one
-function createCanvas() {
-  const etchBorder = document.querySelector(".etch-border");
-  etchBorder.removeChild(document.querySelector(".canvas"));
-
-  const newGrid = document.createElement("div");
-  newGrid.setAttribute("class", "canvas");
-
-  const options = document.querySelector(".options");
-
-  newGrid.style.gridTemplateColumns = `repeat(${sizeSlider.value}, 1fr)`;
-  newGrid.style.gridTemplateRows = `repeat(${sizeSlider.value}, 1fr)`;
-  etchBorder.insertBefore(newGrid, options);
-}
-
 // Updates grid size text
 function updateText() {
   sizeDisplay.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
-}
-
-// Creates divs to fill the canvas based on user selected size
-function setGrid(size) {
-  for (let i = 0; i < Math.pow(size, 2); i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("square");
-    newDiv.addEventListener("mouseover", changeColor);
-    newDiv.addEventListener("mousedown", changeColor);
-    document.querySelector(".canvas").appendChild(newDiv);
-  };
 }
 
 // Creates the initial 16 x 16 grid
 window.onload = () => {
   setGrid(16);
 }
-
-
-
-// gridButton.addEventListener("click", () => {
-//   const etchBorder = document.querySelector(".etchBorder");
-//   etchBorder.removeChild(document.querySelector(".canvas"));
-
-//   const newGrid = document.createElement("div");
-//   newGrid.setAttribute("class", "canvas");
-
-//   const options = document.querySelector(".options");
-  
-//   let gridSize = 0;
-
-//   do {
-//     gridSize = parseInt(prompt("Please enter a new value between 1 and 100"));
-//   } while (gridSize > 100 || gridSize < 1 || !(gridSize));
-
-//   newGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-//   newGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-//   etchBorder.insertBefore(newGrid, options);
-
-//   setGrid(gridSize);
-// })
